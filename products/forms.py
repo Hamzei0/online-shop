f3ab9cefbd7f16bc0a1785fdd03cda75189a2bd6
Mahdi Hamzei <hamzei8434@gmail.com>
2025-09-27 08:52:15 +0330

    show the product comment form

---
+++ products/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from .models import CommentProduct
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CommentProduct
+        fields = ['text', 'stars']
